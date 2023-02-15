778ec70d3b76073a3c38e50925c49cf6fccefd32
Chrislanne Oliveira <chrislanneo@gmail.com>
2023-02-14 23:36:02 -0300

    criando models

--- limpa_bem/models.py
+++ limpa_bem/models.py
@@ -1,3 +1,44 @@
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin, Group, Permission
 from django.db import models
 
-# Create your models here.
+
+class Servicos(models.Model):
+    descricao_servico = models.CharField(max_length=50, blank=False, null=False)
+    preco_servico = models.DecimalField(decimal_places=2, blank=False, null=False)
+    disponibilidade = models.BooleanField(default=False)
+
+
+class UsuarioManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('O endereço de email é obrigatório.')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
+
+class Usuario(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    nome = models.CharField(max_length=100)
+    sobrenome = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UsuarioManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['nome', 'sobrenome']
+
+    def get_full_name(self):
+        return f'{self.nome} {self.sobrenome}'
+
+    def get_short_name(self):
+        return self.nome
